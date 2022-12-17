6948da8ef977e726dbdc744d7c88de6598238910
gontru <104834557+gontru@users.noreply.github.com>
2022-12-16 18:31:30 -0600

    just copied walk through need to attempt on my own

--- tests/02_looping/looping.js
+++ tests/02_looping/looping.js
@@ -1,20 +1,43 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable no-unused-vars */
 function factorial(num) {
-
-}
+  let total = 1;
+  for (let i = 1; i <= num; i++){
+   total *= i;
+} return total;
+} 
 
 function stringReverse(str) {
+ let newStr = '';
+ for (let i = str.Length -1;i >=0; i--)
+ { newStr += str[i]
 
+ } return newStr;
 }
 
 // do not use built in slice method.
 function slicer(originalString, startIdx, endIdx) {
-
+  let newStr =''
+    if (startIdx === undefined){
+      return originalString;
+    }
+    else if (endIdx === undefined){
+    endIdx = originalString.Length;
+}
+    for (let i = startIdx; i< endIdx; i++){
+      newStr += originalString[i];
+    } return newStr;
 }
 
 function addTheEvens(num) {
-
+let total1 = 0;
+ for (let i =0; i<= num; i += 2){
+  total1 += i;
+//   if (i % 2 === 0) {
+//     total += i
+//   }
+//  } return total;
+} return total1;
 }
 
 module.exports = {
